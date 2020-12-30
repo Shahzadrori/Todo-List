9022ec18318a3a976bdbce55fc78693cbd95208b
Shahzad <shahzadrori@gmail.com>
2020-12-30 21:35:19 +0500

    creating initial component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import List from './todo';
 ReactDom.render(
   <>
-  
+  <List />
   </>,document.getElementById('root')
 )
--- src/todo.jsx
+++ src/todo.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 const List = () => {
-    return
+
+
+
+    return(
+    <>
+        <div className='outer'>
+            <div className='inner'>
+            <br/>
+                <h1>Todo List</h1>
+                <br/>
+                <input type='text' placeholder='Enter the task' required />
+                <button type='submit'>+</button>
+            </div>
+        </div>
+    </>
+    )
 }
+export default List;
