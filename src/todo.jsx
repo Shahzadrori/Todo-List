b7be1ef9f9050c7720527f328255aebc882d56a5
Shahzad <shahzadrori@gmail.com>
2020-12-31 12:54:52 +0500

    making script for todo and updating css

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './todo.css';
 
 const List = () => {
+    const [name,setname] = useState();
+
+    const Inp = (val) => {
+        setname(val.target.value);
+    }
 
 
 
@@ -11,11 +16,11 @@ const List = () => {
             <div className='inner'>
                 <h1>Todo List</h1>
                 <div className='btn'>
-                <input type='text' placeholder='Enter the task' required />
+                <input type='text' placeholder='Enter the task' onChange = {Inp}required />
                 <button type='submit'>+</button> 
                 </div>
                 <ol>
-                    <li>Apple</li>
+                    <li>{name}</li>
                 </ol>
             </div>
         </div>
